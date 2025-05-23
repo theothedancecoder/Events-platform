3e7f7190dc5f875a1449a8d71379fbf6096bff0c
theothecoder <theothecoder@gmail.com>
2025-05-23 02:20:29 +0200

    fixed typescript error in webhook

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -2,8 +2,9 @@ import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
 import { clerkClient } from '@clerk/nextjs/server'
 import { verifyWebhook } from '@clerk/nextjs/webhooks'
 import { NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   try {
     const evt = await verifyWebhook(req)
 
@@ -12,24 +13,26 @@ export async function POST(req: Request) {
 
     if (eventType === 'user.created') {
         const {id, email_addresses, image_url, first_name, last_name, username} = evt.data
-        const user ={
-        clerkId :id,
-        email: email_addresses[0].email_address,
-        username: username!,
-        firstName: first_name!,
-        lastName: last_name!,
-        photo: image_url,}
+        const user = {
+          clerkId: id,
+          email: email_addresses[0].email_address,
+          username: username!,
+          firstName: first_name!,
+          lastName: last_name!,
+          photo: image_url,
+        }
     
         const newUser = await createUser(user)
 
-        if(newUser){
-          await clerkClient.users.updateUserMetadata(id,{
-            publicMetadata:{
+        if(newUser) {
+          const clerk = await clerkClient()
+          await clerk.users.updateUserMetadata(id, {
+            publicMetadata: {
               userId: newUser._id
             }
           })
         }
-        return NextResponse.json({message: 'ok', user:newUser})
+        return NextResponse.json({message: 'ok', user: newUser})
     }
 
     if (eventType === 'user.updated') {
@@ -56,9 +59,8 @@ export async function POST(req: Request) {
     }
    
     return new Response('', { status: 200 })
+  } catch (err) {
+    console.error('Error verifying webhook:', err)
+    return new Response('Error verifying webhook', { status: 400 })
   }
- catch (err) {
-  console.error('Error verifying webhook:', err)
-  return new Response('Error verifying webhook', { status: 400 })
-}
 }
